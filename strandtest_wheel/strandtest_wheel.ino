a331c463bdc3a8a867f120bbf921bf3c51e50b7e
anechaev <anechaev@tsconsulting.com>
2021-11-11 22:04:32 +0300

    add yield();

--- strandtest_wheel/strandtest_wheel.ino
+++ strandtest_wheel/strandtest_wheel.ino
@@ -30,7 +30,7 @@ ESP8266WiFiMulti WiFiMulti;
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(243, 4, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(240, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(247, PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
